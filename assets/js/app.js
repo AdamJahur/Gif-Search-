91b45a341e41b84ca30b9c768eb9c45c26848e42
Adam Jahur <adam_jahur@yahoo.com>
2016-11-03 16:45:30 -0400

    finsihed js and app is working. minor bug with the pause funcionfor gifs

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,18 +37,53 @@ $('#movieSearchBTN').on('click', function(){
 });
 
 //send request for gifs
+$('#buttonHolder').on('click', '.movie', function(){
+	$('#gifDiv').empty();
+	var movieData = $(this).data('name');
+	var movieURL = "http://api.giphy.com/v1/gifs/search?q=" + movieData + "+movie&api_key=dc6zaTOxFJmzC&limit=10";
 
-//create div
 
+	$.ajax({url: movieURL, method: 'GET'}).done(function(response){
+		var results = response.data;
+//creat div
+		for (var i = 0; i < results.length; i++){
+			var movieDiv = $('<div>');
+			movieDiv.addClass('movieHolder');
 //get movie rating
-
-//get movie images
-
-//append elements to movie div
-
+			var movieRating = $('<div>');
+			movieRating.addClass('movieHeading');
+			movieRating.text('Rating: ' + results[i].rating);
+//get movie image
+			var movieImg = $('<img>');
+			movieImg.addClass('gifClass');
+			movieImg.attr('src', results[i].images.fixed_height.url);
+			movieImg.attr('data-still', results[i].images.fixed_height.url);
+			movieImg.attr('data-animate', results[i].images.fixed_height.url);
+			movieImg.attr('data-state', 'animate');
+			movieImg.attr('alt', 'movie gif');
+//move elements to movie div
+			movieDiv.append(movieRating);
+			movieDiv.append(movieImg);
+			$('#gifDiv').append(movieDiv);
+		}
 //warning message if not gif exist
-
-//add active class to buttons
+		if(results.length == 0) {
+			$('#gifDiv').append("<p class='warning'>I'm Sorry. No gifs currently exsit for this search</p><p class='warning'>Please try another movie</p>");
+		}
+	});
+// add active class to buttons
+	$(this).siblings().removeClass('active1')
+	$(this).addClass('active1');
+ });
 
 //pause image on click
-
+$('#gifDiv').on('click', '.gifClass', function(){
+	var state = $(this).attr('data-state');
+	if (state == 'still'){
+		$(this).attr('src', $(this).data('animate'));
+		$(this).attr('data-state', 'animate');
+	}else{
+		$(this).attr('src', $(this).data('still'));
+		$(this).attr('data-state', 'still');
+	}
+});
